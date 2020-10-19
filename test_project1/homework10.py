b85452b4f7ac318232cc6b9b73aff60545651d9c
Kseniya <ksusd800@gmail.com>
2020-10-19 18:33:50 +0300

    add ie+ff for hw10

--- test_project1/homework10.py
+++ test_project1/homework10.py
@@ -6,6 +6,8 @@ from selenium import webdriver
 @pytest.fixture
 def driver(request):
     wd = webdriver.Chrome()
+    #wd = webdriver.Firefox()
+    #wd = webdriver.Ie()
     wd.implicitly_wait(5)
     request.addfinalizer(wd.quit)
     return wd
